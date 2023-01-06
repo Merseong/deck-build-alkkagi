37c0c20d48b163b668f4c059adcd5000c93b4abf
milk__way <joming1418@gmail.com>
2023-01-06 15:27:01 +0900

    Update Card Script (Scriptable Object)

--- deck-build-alkkagi/Assets/Scripts/InGameScript/Card.cs
+++ deck-build-alkkagi/Assets/Scripts/InGameScript/Card.cs
@@ -1,15 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Card : MonoBehaviour
+[CreateAssetMenu(fileName = "Card Data",menuName = "Scriptable Object/Card Data")]
+public class Card : ScriptableObject
 {
     // 고유ID
+    [SerializeField]
+    private int cardID;
+    public int CardID { get { return cardID; } }
     // 크기
+    public enum StoneSize
+    {
+        Small,
+        Medium,
+        Large,
+        SuperLarge
+    }
+    public StoneSize stoneSize;
     // 무게
+    public enum StoneWeight
+    {
+        Light,
+        Standard,
+        Heavy
+    }
+    public StoneWeight stoneWeight;
     // 비용
+    public int cardCost;
     // 덱 포함 개수
-
+    public int inDeckNumber;
     // 스프라이트
+    public Sprite sprite;
     // 설명
+    public string description;
 }
