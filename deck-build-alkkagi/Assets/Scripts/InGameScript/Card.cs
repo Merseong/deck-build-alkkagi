6b405c8d331e3eb719c924aa765b623a4e001c0b
milk__way <joming1418@gmail.com>
2023-01-06 16:49:56 +0900

    Rename Card to CardData

--- deck-build-alkkagi/Assets/Scripts/InGameScript/Card.cs
+++ deck-build-alkkagi/Assets/Scripts/InGameScript/Card.cs
@@ -1,35 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Card Data",menuName = "Scriptable Object/Card Data")]
-public class Card : ScriptableObject
+public class Card : MonoBehaviour
 {
-    // 고유ID
-    [SerializeField]
-    private int cardID;
-    public int CardID { get { return cardID; } }
-    // 크기
-    public enum StoneSize
-    {
-        Small,
-        Medium,
-        Large,
-        SuperLarge
-    }
-    public StoneSize stoneSize;
-    // 무게
-    public enum StoneWeight
-    {
-        Light,
-        Standard,
-        Heavy
-    }
-    public StoneWeight stoneWeight;
-    // 비용
-    public int cardCost;
-    // 덱 포함 개수
-    public int inDeckNumber;
-    // 스프라이트
-    public Sprite sprite;
-    // 설명
-    public string description;
+    private CardData cardData;
+    public CardData CardData { set { cardData = value; } }
 }
---
+++ deck-build-alkkagi/Assets/Scripts/InGameScript/CardData.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Card Data",menuName = "Scriptable Object/Card Data")]
+public class CardData : ScriptableObject
+{
+    // 고유ID
+    [SerializeField]
+    private int cardID;
+    public int CardID { get { return cardID; } }
+    // 크기
+    public enum StoneSize
+    {
+        Small,
+        Medium,
+        Large,
+        SuperLarge
+    }
+    public StoneSize stoneSize;
+    // 무게
+    public enum StoneWeight
+    {
+        Light,
+        Standard,
+        Heavy
+    }
+    public StoneWeight stoneWeight;
+    // 비용
+    public int cardCost;
+    // 덱 포함 개수
+    public int inDeckNumber;
+    // 스프라이트
+    public Sprite sprite;
+    // 설명
+    public string description;
+}
