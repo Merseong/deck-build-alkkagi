baa6d8229b33e10bbde7d84c0cec9940d9df3e5a
Merseong <esc990720@korea.ac.kr>
2023-01-27 13:12:47 +0900

    Change receive functions to delegate

--- Assets/Scripts/Network/MyNetworkData.cs
+++ Assets/Scripts/Network/MyNetworkData.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
@@ -285,6 +287,167 @@ namespace MyNetworkData
         private int GetBodySize()
         {
             return BitConverter.ToInt16(mHeaderBuffer, 0);
+        }
+    }
+
+    public class SocketClient
+    {
+        Socket m_socket;
+
+        /// For receive data
+        SocketAsyncEventArgs m_receive_event_args;
+        MessageResolver m_message_resolver;
+        LinkedList<Packet> m_receive_packet_list;
+        private object m_mutex_receive_packet_list = new object();
+        byte[] m_receive_buffer;
+
+        void InitReceive()
+        {
+            m_receive_packet_list = new LinkedList<Packet>();
+            m_receive_buffer = new byte[4096]; // socket buffer size
+            m_message_resolver = new MessageResolver();
+
+            m_receive_event_args = new SocketAsyncEventArgs();
+            m_receive_event_args.Completed += OnReceiveCompleted;
+            m_receive_event_args.UserToken = this;
+            m_receive_event_args.SetBuffer(m_receive_buffer, 0, 1024 * 4);
+        }
+
+        public void Connect(string address, int port)
+        {
+            // TCP 통신
+            m_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // 그때그때 전송
+            m_socket.NoDelay = true;
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+            // 비동기 접속
+            SocketAsyncEventArgs eventArgs = new SocketAsyncEventArgs();
+            eventArgs.Completed += OnConnected;
+            eventArgs.RemoteEndPoint = endPoint;
+
+            bool pending = m_socket.ConnectAsync(eventArgs);
+
+            if (!pending)
+            {
+                OnConnected(null, eventArgs);
+            }
+        }
+
+        void StartReceive()
+        {
+            bool pending = m_socket.ReceiveAsync(m_receive_event_args);
+            if (!pending)
+            {
+                OnReceiveCompleted(this, m_receive_event_args);
+            }
+        }
+
+        public void Send(Packet packet)
+        {
+            if (m_socket == null || !m_socket.Connected)
+            {
+                return;
+            }
+
+            // 이부분은 풀로 만들어서 써도 됨, SocketAsyncEventArgsPool.Instance.Pop();
+            SocketAsyncEventArgs send_event_args = new SocketAsyncEventArgs();
+            if (send_event_args == null)
+            {
+                Debug.LogError("new SocketAsyncEventArgs() result is null");
+                return;
+            }
+
+            send_event_args.Completed += OnSendCompleted;
+            send_event_args.UserToken = this;
+
+            byte[] send_data = packet.GetSendBytes();
+            send_event_args.SetBuffer(send_data, 0, send_data.Length);
+
+            bool pending = m_socket.SendAsync(send_event_args);
+            if (!pending)
+            {
+                OnSendCompleted(null, send_event_args);
+            }
+        }
+
+        void OnConnected(object sender, SocketAsyncEventArgs e)
+        {
+            if (e.SocketError == SocketError.Success) // 연결성공
+            {
+                Debug.Log("hello network!");
+                InitReceive();
+                StartReceive();
+
+                // Send test data
+                var toSend = new TestPacket();
+                toSend.message = "hello world!";
+                var sendPacket = new Packet();
+                sendPacket.Type = (short)PacketType.PACKET_TEST;
+                var toSendSerial = toSend.Serialize();
+                sendPacket.SetData(toSendSerial, toSendSerial.Length);
+                Send(sendPacket);
+            }
+            else // 연결 실패
+            {
+                Debug.LogError("Failed");
+            }
+        }
+
+        void OnReceiveCompleted(object sender, SocketAsyncEventArgs e)
+        {
+            if (e.BytesTransferred > 0 && e.SocketError == SocketError.Success) // 수신 성공
+            {
+                m_message_resolver.OnReceive(e.Buffer, e.Offset, e.BytesTransferred, OnMessageCompleted);
+                StartReceive();
+            }
+            else // 수신 실패
+            {
+                Debug.LogError(e.SocketError);
+            }
+        }
+
+        void OnMessageCompleted(Packet packet)
+        {
+            PushPacket(packet);
+        }
+
+        void PushPacket(Packet packet)
+        {
+            lock (m_mutex_receive_packet_list)
+            {
+                m_receive_packet_list.AddLast(packet);
+            }
+        }
+
+        public void ProcessPackets(NetworkManager.ParsePacketDelegate parser)
+        {
+            lock (m_mutex_receive_packet_list)
+            {
+                foreach (Packet packet in m_receive_packet_list)
+                {
+                    parser(packet);
+                }
+                m_receive_packet_list.Clear();
+            }
+        }
+
+        void OnSendCompleted(object sender, SocketAsyncEventArgs e)
+        {
+            if (e.SocketError == SocketError.Success) // 전송 성공
+            {
+                Debug.Log("Done send");
+            }
+            else // 전송 실패
+            {
+
+            }
+
+            //사용했던 SocketAsyncEventArgs객체를 풀에 다시 넣어준다.
+            //풀에 넣기 전에 해당 객체를 초기화 해 준다.
+            e.Completed -= OnSendCompleted;
+            //SocketAsyncEventArgsPool.Instance.Push(e);
         }
     }
 }
--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -5,17 +5,22 @@ using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using MyNetworkData;
 
 public class NetworkManager : SingletonBehavior<NetworkManager>
 {
+
     [SerializeField]
     private bool m_isNetworkMode;
-    MyNetwork.SocketClient m_client;
-    public MyNetwork.SocketClient Client => m_client;
+    SocketClient m_client;
+    public SocketClient Client => m_client;
 
     [SerializeField]
     private int networkId = -1;
     public int NetworkId => networkId;
+
+    public delegate void ParsePacketDelegate(Packet packet);
+    public ParsePacketDelegate ParsePacket;
 
     [SerializeField]
     private string m_messageToSend;
@@ -37,32 +42,41 @@ public class NetworkManager : SingletonBehavior<NetworkManager>
 
     private void Awake()
     {
+        if (!m_isNetworkMode) return;
+
         m_networkTestCanvas.SetActive(m_isNetworkMode);
+        ParsePacket = new ParsePacketDelegate((_) => { });
 
         // syncVarDataDict = new Dictionary<uint, byte[]>();
         // syncVarActionDict = new Dictionary<uint, Action>();
         syncVarDict = new Dictionary<uint, (byte[], Action)>();
     }
 
-    private void FixedUpdate()
+    private void Start()
     {
-        if (m_client != null)
-        {
-            m_client.ProcessPackets();
-        } 
+        ParsePacket += ParsePacketAction;
     }
 
-    public void SetNetworkId(int id)
+    private void LateUpdate()
     {
+        if (m_isNetworkMode && m_client != null)
+        {
+            m_client.ProcessPackets(ParsePacket);
+        }
+    }
+
+    public void SetNetworkId(int id, bool reset = false)
+    {
+        if (reset)
+        {
+            networkId = -1;
+            return;
+        }
         if (networkId > 0) return;
         networkId = id;
     }
 
-    public void ResetNetworkId()
-    {
-        networkId = -1;
-    }
-
+    #region Send Actions
     public void InitSyncVar(uint netID, byte[] data, Action callback)
     {
         if (syncVarDict.ContainsKey(netID)) return;
@@ -79,8 +93,16 @@ public class NetworkManager : SingletonBehavior<NetworkManager>
             return;
         }
 
-        m_client = new MyNetwork.SocketClient();
+        m_client = new SocketClient();
         m_client.Connect("127.0.0.1", 3333);
+    }
+
+    public void DisconnectServer()
+    {
+        if (!m_isNetworkMode)
+        {
+            return;
+        }
     }
 
     public void EnterGameRoom()
@@ -90,11 +112,11 @@ public class NetworkManager : SingletonBehavior<NetworkManager>
             return;
         }
 
-        var toSend = new MyNetworkData.MessagePacket();
+        var toSend = new MessagePacket();
         toSend.senderID = networkId;
         toSend.message = "ENTER";
-        var sendPacket = new MyNetworkData.Packet().Pack(MyNetworkData.PacketType.ROOM_CONTROL, toSend);
-        m_client.Send(sendPacket);
+        var sendPacket = new Packet().Pack(PacketType.ROOM_CONTROL, toSend);
+        Client.Send(sendPacket);
     }
 
     public void SendMessageToOpponent()
@@ -104,12 +126,99 @@ public class NetworkManager : SingletonBehavior<NetworkManager>
             return;
         }
 
-        var toSend = new MyNetworkData.MessagePacket();
+        var toSend = new MessagePacket();
         toSend.senderID = networkId;
         toSend.message = m_messageToSend;
-        var sendPacket = new MyNetworkData.Packet().Pack(MyNetworkData.PacketType.ROOM_OPPONENT, toSend);
-        m_client.Send(sendPacket);
+        var sendPacket = new Packet().Pack(PacketType.ROOM_OPPONENT, toSend);
+        Client.Send(sendPacket);
 
         m_messageToSend += "a";
     }
+    #endregion
+
+    #region Receive Actions
+    public void ParsePacketAction(Packet packet)
+    {
+        switch ((PacketType)packet.Type)
+        {
+            case PacketType.PACKET_TEST:
+                ParseTestPacket(packet);
+                break;
+            case PacketType.PACKET_INFO:
+                var mp = MessagePacket.Deserialize(packet.Data);
+                NetworkManager.Inst.SetNetworkId(mp.senderID);
+                break;
+            case PacketType.ROOM_CONTROL:
+                CheckMessageEntered(packet);
+                InitServerSyncVar();
+                break;
+            case PacketType.ROOM_OPPONENT:
+                ParseMessagePacket(packet);
+                break;
+            case PacketType.SYNCVAR_CHANGE:
+                ParseSyncVarPacket(packet);
+                break;
+        }
+    }
+
+    private void ParseTestPacket(Packet packet)
+    {
+        var tp = TestPacket.Deserialize(packet.Data);
+
+        Debug.Log("from server - " + tp.message);
+    }
+
+    private void ParseMessagePacket(Packet packet)
+    {
+        MessagePacket mp = MessagePacket.Deserialize(packet.Data);
+
+        Debug.LogWarning("from " + mp.senderID + " - " + mp.message);
+    }
+
+    private void CheckMessageEntered(Packet packet)
+    {
+        MessagePacket mp = MessagePacket.Deserialize(packet.Data);
+        var arr = mp.message.Split(" ");
+        if (arr[1] == "ENTERED")
+        {
+            Debug.Log($"from server - room {arr[0]} matched!");
+            var toSend = new MessagePacket();
+            toSend.senderID = NetworkManager.Inst.NetworkId;
+            toSend.message = "LOADED";
+            var sendPacket = new Packet().Pack(PacketType.ROOM_CONTROL, toSend);
+            Client.Send(sendPacket);
+        }
+        else if (arr[1] == "START")
+        {
+            Debug.Log($"from server - game start! with first player {arr[0]}");
+        }
+    }
+
+    private void ParseSyncVarPacket(Packet packet)
+    {
+        var sp = SyncVarPacket.Deserialize(packet.Data);
+
+        if (!NetworkManager.Inst.SyncVarDict.ContainsKey(sp.NetID)) return;
+
+        var (_data, _callback) = NetworkManager.Inst.SyncVarDict[sp.NetID];
+        _data = sp.Data;
+        NetworkManager.Inst.SyncVarDict[sp.NetID] = (_data, _callback);
+        _callback();
+    }
+
+    private void InitServerSyncVar()
+    {
+        foreach (var kv in NetworkManager.Inst.SyncVarDict)
+        {
+            var syncPacket = new SyncVarPacket();
+            syncPacket.NetID = kv.Key;
+            syncPacket.Data = kv.Value.data;
+            var data = syncPacket.Serialize();
+            var p = new Packet();
+            p.Type = (Int16)PacketType.SYNCVAR_INIT;
+            p.SetData(data, data.Length);
+            Client.Send(p);
+        }
+    }
+    #endregion
 }
