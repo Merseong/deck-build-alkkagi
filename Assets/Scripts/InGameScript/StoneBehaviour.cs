a2988d4e971d47dd924128204bbcac4164c139b0
FrozenThera <dongmin11004@korea.ac.kr>
2023-02-08 05:31:08 +0900

    Add direct & indirect exit
    Add centerline
    Change inform panel background alpha

--- Assets/Scripts/InGameScript/StoneBehaviour.cs
+++ Assets/Scripts/InGameScript/StoneBehaviour.cs
@@ -42,6 +42,8 @@ public class StoneBehaviour : MonoBehaviour
     [SerializeField] private GameManager.PlayerEnum belongingPlayer;
     public GameManager.PlayerEnum BelongingPlayer => belongingPlayer;
 
+    private bool isExiting = false;
+    [SerializeField] float indirectExitTime;
     private void Start()
     {
         boardTransform = GameObject.Find("Board").transform;
@@ -53,7 +55,7 @@ public class StoneBehaviour : MonoBehaviour
         if(!CheckStoneDropByTransform())
         {
             RemoveStoneFromGame();
-            Destroy(gameObject);
+            StartCoroutine(EIndirectExit());
         }
         if (akgRigidbody.velocity == Vector3.zero || !isClicked)
         {
@@ -111,5 +113,48 @@ public class StoneBehaviour : MonoBehaviour
         if(transform.position.z > boardTransform.transform.position.z + boardTransform.transform.localScale.z * 5f) return false;
         if(transform.position.z < boardTransform.transform.position.z - boardTransform.transform.localScale.z * 5f) return false;
         return true;
+    }
+
+    private bool isStoneLeaveScreen()
+    {
+        Vector3 screenCenter = new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.transform.position.z);
+        Vector3 screenHeight = new Vector3(Screen.width / 2, Screen.height, Camera.main.transform.position.z);
+        Vector3 screenWidth = new Vector3(Screen.width, Screen.height/2, Camera.main.transform.position.z);
+        Vector3 goscreen = Camera.main.WorldToScreenPoint(transform.position);
+ 
+        float distX = Vector3.Distance(new Vector3(Screen.width / 2, 0f, 0f), new Vector3(goscreen.x, 0f,0f));
+        float distY = Vector3.Distance(new Vector3(0f, Screen.height / 2, 0f), new Vector3(0f, goscreen.y, 0f));
+ 
+        return distX > Screen.width / 2 || distY > Screen.height / 2;
+    }
+
+    private void IndirectExit()
+    {
+        Debug.Log("Stone is Indirectly Exited!");
+        Destroy(gameObject);
+    }
+
+    private void DirectExit()
+    {
+        Debug.Log("Stone is Directly Exited!");
+        Destroy(gameObject);
+    }
+
+    private IEnumerator EIndirectExit()
+    {
+        isExiting = true;
+        float curTime = indirectExitTime;
+        while(curTime >= 0)
+        {
+            if(isStoneLeaveScreen())
+            {
+                DirectExit();
+                yield break;
+            }
+            curTime -= Time.deltaTime;
+            transform.localScale = new Vector3(curTime/indirectExitTime, 1f, curTime/indirectExitTime);
+            yield return null;
+        }
+        IndirectExit();
     }
 }
