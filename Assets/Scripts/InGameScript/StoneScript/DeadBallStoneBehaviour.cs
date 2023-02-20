d55222e10d3cc6711f1849af424639414975e31f
milk__way <joming1418@gmail.com>
2023-02-21 02:46:47 +0900

    DeadBall stone

---
+++ Assets/Scripts/InGameScript/StoneScript/DeadBallStoneBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadBallStoneBehaviour : StoneBehaviour
+{
+    public override void OnEnter()
+    {
+        OnHit += (akg) =>
+        {
+            StoneBehaviour stone = akg.gameObject.GetComponent<StoneBehaviour>();
+            foreach (var property in stone.Properties)
+            {
+                if (property is CursedProperty)
+                {
+                    stone.RemoveStoneFromGame();
+                }
+            }
+        };
+    }
+}
