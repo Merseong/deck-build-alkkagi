1e1d8e59901506e333b347c3572078660a9a7be6
milk__way <joming1418@gmail.com>
2023-02-21 02:01:14 +0900

    Warlock stone

---
+++ Assets/Scripts/InGameScript/StoneScript/WarlockStoneBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarlockStoneBehaviour : StoneBehaviour
+{
+    public override void OnEnter()
+    {
+        List<StoneBehaviour> oppoStones = new List<StoneBehaviour>();
+        foreach (var stone in GameManager.Inst.AllStones.Values)
+        {
+            if (stone.BelongingPlayer == GameManager.PlayerEnum.OPPO)
+            {
+                oppoStones.Add(stone);
+            }
+        }
+        int randNum = UnityEngine.Random.Range(0, oppoStones.Count);
+        oppoStones[randNum].AddProperty(new CursedProperty(oppoStones[randNum]));
+    }
+}
