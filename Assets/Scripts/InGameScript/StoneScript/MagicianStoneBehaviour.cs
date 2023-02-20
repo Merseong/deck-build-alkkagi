406cdf0f58be06c9aa529630af8a546db14d6178
Jaehyung Doh <knuppe22@gmail.com>
2023-02-20 16:37:15 +0900

    magician stone

---
+++ Assets/Scripts/InGameScript/StoneScript/MagicianStoneBehaviour.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicianStoneBehaviour: StoneBehaviour
+{
+    public override void InitProperty()
+    {
+        base.InitProperty();
+
+        Properties.Add(new ShieldProperty(this));
+    }
+}
