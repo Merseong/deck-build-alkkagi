5369fdb60e9120ac71bd6737363a4aac54118800
milk__way <joming1418@gmail.com>
2023-02-21 02:26:27 +0900

    Priest stone

---
+++ Assets/Scripts/InGameScript/StoneScript/PriestStoneBehaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PriestStoneBehaviour : StoneBehaviour
+{
+    public override void OnEnter()
+    {
+        PlayerBehaviour player = GameManager.Inst.players[(int)BelongingPlayer];
+        float minDist = 10000f;
+        StoneBehaviour minDistStone = null;
+        foreach (StoneBehaviour stone in player.Stones.Values)
+        {
+            if (stone != this)
+            {
+                float dist = Vector3.Distance(transform.position, stone.gameObject.transform.position);
+                if (dist < minDist)
+                {
+                    minDistStone = stone;
+                    minDist = dist;
+                }
+            }
+        }
+        if (minDistStone != null)
+        {
+            minDistStone.AddProperty(new ShieldProperty(minDistStone));
+        }
+    }
+}
