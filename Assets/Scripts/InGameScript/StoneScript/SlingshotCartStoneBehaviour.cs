659211303bcf46e6f0a9269b746ee3b8fd5e8fc4
Jaehyung Doh <knuppe22@gmail.com>
2023-02-22 22:42:25 +0900

    slingshot fix?

--- Assets/Scripts/InGameScript/StoneScript/SlingshotCartStoneBehaviour.cs
+++ Assets/Scripts/InGameScript/StoneScript/SlingshotCartStoneBehaviour.cs
@@ -11,7 +11,7 @@ public class SlingshotCartStoneBehaviour : StoneBehaviour
         {
             if(!options.Equals(""))
             {
-                BelongingPlayer.OnTurnEnd += SlingShot_Oppo;
+                //BelongingPlayer.OnTurnEnd += SlingShot_Oppo;
             }
         }
         else
@@ -24,56 +24,59 @@ public class SlingshotCartStoneBehaviour : StoneBehaviour
     public override void OnExit(bool calledByPacket = false, string options = "")
     {
         BelongingPlayer.OnTurnEnd -= SlingShot;
-        BelongingPlayer.OnTurnEnd -= SlingShot_Oppo;
+        //BelongingPlayer.OnTurnEnd -= SlingShot_Oppo;
 
         base.OnExit(calledByPacket, options);
     }
     private void SlingShot()
     {
-        List<Card> handCard = BelongingPlayer.GetHandCard();
-        List<Card> oneCostCard = new List<Card>();
-        foreach (Card card in handCard)
+        if (BelongingPlayerEnum == GameManager.PlayerEnum.LOCAL)
         {
-            if (card.CardData.cardCost == 1)
+            List<Card> handCard = BelongingPlayer.GetHandCard();
+            List<Card> oneCostCard = new List<Card>();
+            foreach (Card card in handCard)
             {
-                oneCostCard.Add(card);
+                if (card.CardData.cardCost == 1)
+                {
+                    oneCostCard.Add(card);
+                }
             }
+
+            if (oneCostCard.Count <= 0)
+            {
+                return;
+            }
+
+            int randNum = Random.Range(0, oneCostCard.Count);
+
+            Card oneCard = oneCostCard[randNum];
+            CardData cardData = oneCard.CardData;
+
+            var stoneID = GameManager.Inst.LocalPlayer.SpawnStone(cardData, transform.position, -1, true);
+            StoneBehaviour stone = GameManager.Inst.FindStone(stoneID);
+
+
+            var spawnAkg = stone.GetComponent<AkgRigidbody>();
+            var thisAkg = GetComponent<AkgRigidbody>();
+            spawnAkg.IgnoreCollide.Add(thisAkg);
+            thisAkg.IgnoreCollide.Add(spawnAkg);
+
+            GameManager.Inst.LocalPlayer.RemoveHandCardAndArrange(oneCard, transform.position, stoneID);
+
+            LocalPlayerBehaviour local = GameManager.Inst.LocalPlayer as LocalPlayerBehaviour;
+            bool isRotated = (BelongingPlayerEnum == GameManager.PlayerEnum.LOCAL) == local.IsLocalRotated;
+            Vector3 shootVel = isRotated ? new Vector3(0, 0, -1500) : new Vector3(0, 0, 1500);
+            stone._Shoot(shootVel * stone.GetComponent<AkgRigidbody>().Mass, isRotated);
         }
-        
-        if (oneCostCard.Count <= 0)
-        {
-            return;
-        }
-
-        int randNum = Random.Range(0, oneCostCard.Count);
-
-        Card oneCard = oneCostCard[randNum];
-        CardData cardData = oneCard.CardData;
-
-        var stoneID = GameManager.Inst.LocalPlayer.SpawnStone(cardData, transform.position, -1, true);
-        StoneBehaviour stone = GameManager.Inst.FindStone(stoneID);
-        
-
-        var spawnAkg = stone.GetComponent<AkgRigidbody>();
-        var thisAkg = GetComponent<AkgRigidbody>();
-        spawnAkg.IgnoreCollide.Add(thisAkg);
-        thisAkg.IgnoreCollide.Add(spawnAkg);
-
-        GameManager.Inst.LocalPlayer.RemoveHandCardAndArrange(oneCard, transform.position, stoneID);
-        
-        LocalPlayerBehaviour local = GameManager.Inst.LocalPlayer as LocalPlayerBehaviour;
-        bool isRotated = (BelongingPlayerEnum == GameManager.PlayerEnum.LOCAL) == local.IsLocalRotated;
-        Vector3 shootVel = isRotated ? new Vector3(0, 0, -1500) : new Vector3(0, 0, 1500);
-        stone._Shoot(shootVel*stone.GetComponent<AkgRigidbody>().Mass, isRotated);
     }
 
-    private void SlingShot_Oppo()
-    {
-        StoneBehaviour stone = GameManager.Inst.FindStone(int.Parse(option));
+    //private void SlingShot_Oppo()
+    //{
+    //    StoneBehaviour stone = GameManager.Inst.FindStone(int.Parse(option));
 
-        var spawnAkg = stone.GetComponent<AkgRigidbody>();
-        var thisAkg = GetComponent<AkgRigidbody>();
-        spawnAkg.IgnoreCollide.Add(thisAkg);
-        thisAkg.IgnoreCollide.Add(spawnAkg);   
-    }
+    //    var spawnAkg = stone.GetComponent<AkgRigidbody>();
+    //    var thisAkg = GetComponent<AkgRigidbody>();
+    //    spawnAkg.IgnoreCollide.Add(thisAkg);
+    //    thisAkg.IgnoreCollide.Add(spawnAkg);
+    //}
 }
