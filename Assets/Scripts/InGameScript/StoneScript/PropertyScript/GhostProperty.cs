920d5bceee82313e93c9a22c68ad6ab2066838ac
Jaehyung Doh <knuppe22@gmail.com>
2023-02-23 01:59:18 +0900

    ghost is now real ghost

--- Assets/Scripts/InGameScript/StoneScript/PropertyScript/GhostProperty.cs
+++ Assets/Scripts/InGameScript/StoneScript/PropertyScript/GhostProperty.cs
@@ -6,5 +6,19 @@ public class GhostProperty : StoneProperty
 {
     public GhostProperty(StoneBehaviour stone, int turn = -1) : base(stone, turn) { }
 
+    public override void OnAdded(bool isReplaced = false)
+    {
+        base.OnAdded(isReplaced);
+
+        baseStone.GetComponent<AkgRigidbody>().layerMask |= AkgLayerMask.GHOST;
+    }
+
+    public override void OnRemoved(bool isReplaced = false)
+    {
+        base.OnRemoved(isReplaced);
+
+        baseStone.GetComponent<AkgRigidbody>().layerMask &= ~AkgLayerMask.GHOST;
+    }
+
     public override bool IsGhost(bool value) { return value || (remainingTurn != 0); }
 }
