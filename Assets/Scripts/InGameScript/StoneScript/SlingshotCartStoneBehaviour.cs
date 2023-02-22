01339407c27ee1a2eba6427a4949931052d7d248
FrozenThera <dongmin11004@korea.ac.kr>
2023-02-22 22:31:55 +0900

    slingshotcart

--- Assets/Scripts/InGameScript/StoneScript/SlingshotCartStoneBehaviour.cs
+++ Assets/Scripts/InGameScript/StoneScript/SlingshotCartStoneBehaviour.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class SlingshotCartStoneBehaviour : StoneBehaviour
 {
+    string option = "";
     public override void OnEnter(bool calledByPacket = false, string options = "")
     {
-        BelongingPlayer.OnTurnEnd += SlingShot;
-
-        base.OnEnter(calledByPacket, options);
+        if(calledByPacket)
+        {
+            if(!options.Equals(""))
+            {
+                BelongingPlayer.OnTurnEnd += SlingShot_Oppo;
+            }
+        }
+        else
+        {
+            BelongingPlayer.OnTurnEnd += SlingShot;
+        } 
+            
+        base.OnEnter(calledByPacket, option);
     }
     public override void OnExit(bool calledByPacket = false, string options = "")
     {
         BelongingPlayer.OnTurnEnd -= SlingShot;
+        BelongingPlayer.OnTurnEnd -= SlingShot_Oppo;
 
         base.OnExit(calledByPacket, options);
     }
     private void SlingShot()
     {
-        Debug.Log(2);
         List<Card> handCard = BelongingPlayer.GetHandCard();
         List<Card> oneCostCard = new List<Card>();
         foreach (Card card in handCard)
@@ -28,37 +39,41 @@ public class SlingshotCartStoneBehaviour : StoneBehaviour
                 oneCostCard.Add(card);
             }
         }
-        Debug.Log(1);
         
         if (oneCostCard.Count <= 0)
         {
-            Debug.Log(0);
             return;
         }
 
-        int randNum = UnityEngine.Random.Range(0, oneCostCard.Count);
+        int randNum = Random.Range(0, oneCostCard.Count);
 
         Card oneCard = oneCostCard[randNum];
-        SpawnStone(oneCard);
-        //var stoneId = BelongingPlayer.SpawnStone(oneCard.CardData, transform.position,-1,true);
-    }
+        CardData cardData = oneCard.CardData;
 
-    private void SpawnStone(Card card)
-    {
-        CardData cardData = card.CardData;
         var stoneID = GameManager.Inst.LocalPlayer.SpawnStone(cardData, transform.position, -1, true);
         StoneBehaviour stone = GameManager.Inst.FindStone(stoneID);
-        var bulletAkg = stone.GetComponent<AkgRigidbody>();
+        
+
+        var spawnAkg = stone.GetComponent<AkgRigidbody>();
         var thisAkg = GetComponent<AkgRigidbody>();
-        bulletAkg.IgnoreCollide.Add(thisAkg);
-        thisAkg.IgnoreCollide.Add(bulletAkg);
-        GameManager.Inst.LocalPlayer.RemoveHandCardAndArrange(card, transform.position, stoneID);
+        spawnAkg.IgnoreCollide.Add(thisAkg);
+        thisAkg.IgnoreCollide.Add(spawnAkg);
+
+        GameManager.Inst.LocalPlayer.RemoveHandCardAndArrange(oneCard, transform.position, stoneID);
+        
         LocalPlayerBehaviour local = GameManager.Inst.LocalPlayer as LocalPlayerBehaviour;
         bool isRotated = (BelongingPlayerEnum == GameManager.PlayerEnum.LOCAL) == local.IsLocalRotated;
         Vector3 shootVel = isRotated ? new Vector3(0, 0, -1500) : new Vector3(0, 0, 1500);
         stone._Shoot(shootVel*stone.GetComponent<AkgRigidbody>().Mass, isRotated);
+    }
 
-        // TODO: 쏜 stone이 가만히 있음, 호출 시점의 velocity == 0
-        //stone._Shoot(-akgRigidbody.velocity, true);
+    private void SlingShot_Oppo()
+    {
+        StoneBehaviour stone = GameManager.Inst.FindStone(int.Parse(option));
+
+        var spawnAkg = stone.GetComponent<AkgRigidbody>();
+        var thisAkg = GetComponent<AkgRigidbody>();
+        spawnAkg.IgnoreCollide.Add(thisAkg);
+        thisAkg.IgnoreCollide.Add(spawnAkg);   
     }
 }
